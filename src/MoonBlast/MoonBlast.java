a857deed0f09415e91420ad59901bd864d33e50b
Sosmith84 <48396637+Sosmith84@users.noreply.github.com>
2019-04-18 12:20:06 -0500

    Update MoonBlast.java

--- src/MoonBlast/MoonBlast.java
+++ src/MoonBlast/MoonBlast.java
@@ -14,15 +14,25 @@
 package MoonBlast;
 
 import java.io.File;
+import javafx.animation.RotateTransition;
+import javafx.animation.ScaleTransition;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  *
@@ -36,30 +46,154 @@ public class MoonBlast extends Application{
     
     @Override
     public void start(Stage primaryStage){
+//        primaryStage.setTitle("Shoot!");
+//        
+//        String asteroidPic = "Asteroid.png";
+//        
+//        Image asteroid = new Image(new File(asteroidPic).toURI().toString());
+//        ImageView imageView = new ImageView(asteroid);
+//        RotateTransition rT = new RotateTransition();
+//        ScaleTransition sT = new ScaleTransition(Duration.millis(5000));
+//        
+//        
+//        rT.setDuration(Duration.millis(5000));
+//        rT.setNode(imageView);
+//        sT.setNode(imageView);
+//        rT.setByAngle(360);
+//        sT.setByX(1.5);
+//        sT.setByY(1.5);
+//        rT.setCycleCount(5);
+//        sT.setCycleCount(5);
+//        rT.setAutoReverse(false);
+//        sT.setAutoReverse(false);
+//        sT.play();
+//        rT.play();
+//        
+//        imageView.setX(50); 
+//        imageView.setY(25); 
+//      
+//        //setting the fit height and width of the image view 
+//        imageView.setFitHeight(455); 
+//        imageView.setFitWidth(500); 
+//
+//        //Setting the preserve ratio of the image view 
+//        imageView.setPreserveRatio(true);  
+//        
+//        Group root = new Group(imageView);
+//        Scene theScene = new Scene( root, 600,500 );
+//        primaryStage.setScene( theScene );
+//
+//        primaryStage.show();
+//        
+//        
+
         primaryStage.setTitle("Shoot!");
         
         String musicFile = "Laser_Shoot2.wav";
         
         Media sound = new Media(new File(musicFile).toURI().toString());
         MediaPlayer mediaPlayer = new MediaPlayer(sound);
+        MediaView mv = new MediaView(mediaPlayer);
+        
+        Group main = new Group();
+        Scene scene = new Scene(main, 300, 300);
         
         
-        Button button2 = new Button("Shoot");
-        for (int i = 0; i < 10000; i++) {
             
+           
             
         
-        button2.setOnAction(new EventHandler<ActionEvent>() {
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
-            public void handle(ActionEvent event) {
-                mediaPlayer.play();
+            public void handle(KeyEvent t) {
+                switch (t.getCode()){
+                    case Q:
+                        mediaPlayer.stop();
+                        mediaPlayer.play();
+                        break;
+                    case W:
+                        mediaPlayer.stop();
+                        mediaPlayer.play();
+                        break;
+                    case E:
+                        mediaPlayer.stop();
+                        mediaPlayer.play();
+                        break;
+                    case A:
+                        mediaPlayer.stop();
+                        mediaPlayer.play();
+                        break;
+                    case S:
+                        mediaPlayer.stop();
+                        mediaPlayer.play();
+                        break;
+                    case D:
+                        mediaPlayer.stop();
+                        mediaPlayer.play();
+                        break;
+                    case Z:
+                        mediaPlayer.stop();
+                        mediaPlayer.play();
+                        break;
+                    case X:
+                        mediaPlayer.stop();
+                        mediaPlayer.play();
+                        break;
+                    case C:
+                        mediaPlayer.stop();
+                        mediaPlayer.play();
+                        break;
+                    case NUMPAD1:
+                        mediaPlayer.stop();
+                        mediaPlayer.play();
+                        break;
+                    case NUMPAD2:
+                        mediaPlayer.stop();
+                        mediaPlayer.play();
+                        break;
+                    case NUMPAD3:
+                        mediaPlayer.stop();
+                        mediaPlayer.play();
+                        break;
+                    case NUMPAD4:
+                        mediaPlayer.stop();
+                        mediaPlayer.play();
+                        break;
+                    case NUMPAD5:
+                        mediaPlayer.stop();
+                        mediaPlayer.play();
+                        break;
+                    case NUMPAD6:
+                        mediaPlayer.stop();
+                        mediaPlayer.play();
+                        break;
+                    case NUMPAD7:
+                        mediaPlayer.stop();
+                        mediaPlayer.play();
+                        break;
+                    case NUMPAD8:
+                        mediaPlayer.stop();
+                        mediaPlayer.play();
+                        break;
+                    case NUMPAD9:
+                        mediaPlayer.stop();
+                        mediaPlayer.play();
+                        break;
+                }
+                
             }
+            
         });
-        }
         
-        StackPane winder = new StackPane();
-        winder.getChildren().add(button2);
-        primaryStage.setScene(new Scene(winder, 100, 100));
+        
+        
+            
+           
+        
+        
+        main.getChildren().add(mv);
+        primaryStage.setScene(scene);
         primaryStage.show();
+        
     }
 }
